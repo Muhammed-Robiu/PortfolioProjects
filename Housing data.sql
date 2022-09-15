b52ecdac687ee9bfe70627ddc4b45a378a2c627f
Muhammed-Robiu <110641838+Muhammed-Robiu@users.noreply.github.com>
2022-09-15 13:09:09 -0700

    Update Housing data.sql

--- Housing data.sql
+++ Housing data.sql
@@ -1,157 +1,124 @@
-select * from portfolioproject..CovidDeaths;
+-- Alter date format
 
-select * from portfolioproject..Covidvaccination;
+SELECT SaleDateUpdated
+FROM PortProject..NashvilleHousing
 
+ALTER TABLE NashvilleHousing
+ADD SaleDateUpdated Date;
 
---selecting data considered for this project
-
-select location, date, total_cases, new_cases, total_deaths, population
-from portfolioproject..CovidDeaths
-order by 1,2;
-
---comparing total cases vs totat death to show the probability of dying from covid.
-
-select location,  date, total_cases, total_deaths, (total_deaths/total_cases)*100 as [death percetage]
-from portfolioproject..CovidDeaths
-where continent is null
-order by 1,2;
-
-
---displaying death percentage result for afghanistan 
-select location,  date, total_cases, total_deaths, (total_deaths/total_cases)*100 as [death percetage]
-from portfolioproject..CovidDeaths
-where location like '%afghanistan%' and total_deaths is not null
-order by  [death percetage] desc;
-
---displaying death percentage result for Nigeria
-select location,  date, total_cases, total_deaths, (total_deaths/total_cases)*100 as [death percetage]
-from portfolioproject..CovidDeaths
-where location='Nigeria' and total_deaths is not null
-order by  [death percetage] desc;
-
---displaying cases per population percentage result for all locations 
-select location,  date, total_cases, population, (total_cases/population)*100 as [cases per population]
-from portfolioproject..CovidDeaths
-where total_cases is not null and population is not null
-order by  [cases per population] desc;
-
-
---looking at countries with highest infecton rate at a certain date 
-select top(5) location, max(total_cases) as [total cases]
-from portfolioproject..CovidDeaths
-where continent is not null
-group by location
-order by [total cases] desc;
-
---looking at total cases per countries showing top 5
-select top(5) location, sum(total_cases) as [total cases]
-from portfolioproject..CovidDeaths
-where continent is not null
-group by location
-order by [total cases] desc;
-
---looking at total cases per continent compared to population. 
-select location, population, max(total_cases) as [total cases], max((total_cases/population))*100 as [cases per poppulation]
-from portfolioproject..CovidDeaths
-where continent is null
-group by location, population
-order by 4 desc;
-
---showing continent with highest death count at a given date..
-select location, max(cast(total_deaths as int)) as [death rate]
-from portfolioproject..CovidDeaths
-where continent is null
-group by location
-order by [death rate] desc;
-
--- showing results for total date rate per countries.
-select location, sum (cast(total_deaths as int)) as [death rate]
-from portfolioproject..CovidDeaths
-where continent is not null
-group by location
-order by 1 desc;
-
-
---showing continent with highest death rate.
-select continent, max(cast(total_deaths as int)) as [death rate per contintent]
-from portfolioproject..CovidDeaths
-where continent is not null
-group by continent
-order by 2;
-
-
---showing the total death rate per continents.
-select continent, sum(cast(total_deaths as int)) as [death rate per continent]
-from portfolioproject..CovidDeaths
-where continent is not null
-group by continent
-order by 2;
-
-select continent, max(total_cases) as [cases per continent]
-from portfolioproject..CovidDeaths
-where continent is not null
-group by continent
-order by 2;
-
---looking at global cases
-
-select sum(new_cases) as total_cases, sum(convert(int, new_deaths)) as total_deaths,
-sum(convert(int, new_deaths))/sum(new_cases)*100 as death_percentage
-FROM portfolioproject..CovidDeaths
-order by 1,2;
-
-
-select location, date, total_cases, new_cases, total_deaths, population
-from portfolioproject..CovidDeaths
-where location='world' and continent is null
-order by 1,2;
-
-select location, population, sum(cast(total_deaths as int)) as [world death], sum(total_cases) as [total world cases]
-from portfolioproject..CovidDeaths
-where location='world'
-group by location, population
-order by 1,2;
-
-
-select location, population, max(total_cases) as [total cases], max((total_cases/population))*100 as [cases per poppulation]
-from portfolioproject..CovidDeaths
-where location='world'
-group by location, population
-order by 4 desc;
-
-
---looking at total population vs total vaccination
-select cd.continent,cd.location, cd.date, cd.population, cv.new_vaccinations,
-sum(convert(int, cv.new_vaccinations)) over (partition by cd.location, cd.date) as [rolling vaccinatedd]
-from portfolioproject..CovidDeaths cd
-join portfolioproject..CovidVaccination cv
-	on cd.location=cv.location and	cd.date=cv.date
-where cd.continent is not null
-order by 2,3;
+UPDATE NashvilleHousing
+SET SaleDateUpdated = CONVERT(Date, SaleDate)
 
 
 
-
---creating view for later visual 
-create view [death rate per continent] as
-select continent, sum(cast(total_deaths as int)) as [death rate per contintent]
-from portfolioproject..CovidDeaths
-where continent is not null
-group by continent;
-
-create view [cases per continent] as
-select continent, max(total_cases) as [cases per contintent]
-from portfolioproject..CovidDeaths
-where continent is not null
-group by continent;
-
-create view [cases per populatiion] as
-select location,  date, total_cases, population, (total_cases/population)*100 as [cases per population]
-from portfolioproject..CovidDeaths
-where total_cases is not null and population is not null;
+--Populate property address data
 
 
-create view [probability of dying from covid] as
-select location,  date, total_cases, total_deaths, (total_deaths/total_cases)*100 as [death percetage]
-from portfolioproject..CovidDeaths
-where continent is null;
+SELECT a.PropertyAddress, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortProject..NashvilleHousing a
+JOIN PortProject..NashvilleHousing b
+		ON a.ParcelID = b.ParcelID
+		AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+UPDATE a
+SET a.PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortProject..NashvilleHousing a
+JOIN PortProject..NashvilleHousing b
+		ON a.ParcelID = b.ParcelID
+		AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+
+
+--Breaking out address into individual columns (Address, City, State)
+
+SELECT PropertyAddress,
+SUBSTRING(PropertyAddress, 1 ,CHARINDEX(',', PropertyAddress) -1),
+SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 ,LEN(PropertyAddress))	
+FROM PortProject..NashvilleHousing
+
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitAddress VARCHAR(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1 ,CHARINDEX(',', PropertyAddress) -1)	
+
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitCity VARCHAR(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 ,LEN(PropertyAddress))
+
+
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress,',','.'),3),
+PARSENAME(REPLACE(OwnerAddress,',','.'),2),
+PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+FROM PortProject..NashvilleHousing
+
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitAddress VARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress,',','.'),3)
+
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitCity VARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress,',','.'),2)
+
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitState VARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+
+
+
+--Changed Y and S to Yes and No in 'Sold as vacant field'
+
+SELECT DISTINCT(SoldAsVacant), COUNT(SoldAsVacant)
+FROM PortProject..NashvilleHousing
+GROUP BY SoldAsVacant
+
+SELECT SoldAsVacant,
+	CASE WHEN SoldAsVacant = 'Y' THEN 'YES'
+		 WHEN SoldAsVacant = 'N' THEN 'NO'
+		 ELSE SoldAsVacant
+		 END	
+FROM PortProject..NashvilleHousing
+
+UPDATE NashvilleHousing
+SET SoldAsVacant = 	CASE WHEN SoldAsVacant = 'Y' THEN 'YES'
+						 WHEN SoldAsVacant = 'N' THEN 'NO'
+						 ELSE SoldAsVacant
+						 END
+
+
+
+--Remove duplicates
+
+WITH RowNumCTE 
+as(
+SELECT *, 
+	ROW_NUMBER() OVER (PARTITION BY ParcelID,
+									PropertyAddress,
+									SalePrice,
+									SaleDate,
+									LegalReference
+									ORDER BY UniqueID)
+									as row_num
+FROM PortProject..NashvilleHousing
+)
+
+SELECT * 
+FROM RowNumCTE
+ORDER BY row_num DESC
